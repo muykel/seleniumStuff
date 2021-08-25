a9ba58deab9ead73417c1c4f688698b53233cce6
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-08-25 11:51:12 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+# driver initialization
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+# launch URL
+driver.get("https://www.tutorialspoint.com/index.htm")
